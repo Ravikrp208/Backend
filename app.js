75aad55d1bb9fc9e011a16244dfa5afa6a13a3a4
Ravikrp208 <ravikrp208@gmail.com>
2026-01-18 22:47:05 +0530

    tutorial

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-const catME = require ("cat-me")
- console.log (catME())
+const express = require ("express")
+const app= express () // server create kar rahe hai
+
+app.listen(8000) // server ko star kar rahe hai 
