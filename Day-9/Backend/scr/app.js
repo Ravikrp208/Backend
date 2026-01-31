d0126a7efc76be228304f5ebc2ce203999b1127d
Ravikrp208 <ravikrp208@gmail.com>
2026-01-31 22:15:24 +0530

    backend conn

---
+++ Day-9/Backend/scr/app.js
@@ -0,0 +1,91 @@
+/**
+ *server ko create karna 
+ 
+* /
+**/
+
+const express = require("express");
+const noteModel = require("./Models/notes.model");
+
+const app = express();
+app.use(express.json());
+
+/**
+ * POST /notes
+ * req.body => { title, description }
+ */
+
+app.post("/notes", async (req, res) => {
+  const { title, description } = req.body;
+
+  const note = await noteModel.create({
+    title,
+    description,
+  });
+
+  res.status(201).json({
+    message: "Note created successfully",
+    note,
+  });
+});
+/**
+ * post /notes
+ *
+ */
+
+app.get("/notes", async (req, res) => {
+  const notes = await noteModel.find();
+
+  res.status(200).json({
+    message: "Note feached successfully",
+    notes,
+  });
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const deletedNote = await noteModel.findByIdAndDelete(id);
+
+    if (!deletedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    res.status(200).json({
+      message: "Note deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting note",
+      error: error.message,
+    });
+  }
+});
+app.patch("/api/notes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+
+    const updatedNote = await noteModel.findByIdAndUpdate(
+      id,
+      { description },
+      { new: true },
+    );
+
+    res.status(200).json({
+      message: "Note updated successfully",
+      data: updatedNote,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating note",
+      error: error.message,
+    });
+  }
+});
+
+
+
+
+module.exports = app;
---
+++ Day-9/Backend/server.js
@@ -0,0 +1,11 @@
+
+require("dotenv").config ()
+const connectTodb = require("./scr/config/database");
+const app = require("./scr/app");
+
+
+ connectTodb ()
+
+app.listen(3000, () => {
+  console.log("server is runnig on port 3000");
+});
