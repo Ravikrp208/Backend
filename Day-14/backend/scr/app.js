669c3337dde08281792d531d45edb9b22e2d8be0
Ravikrp208 <ravikrp208@gmail.com>
2026-02-19 23:54:51 +0530

    login Ui

--- Day-14/backend/scr/app.js
+++ Day-14/backend/scr/app.js
@@ -1,15 +1,22 @@
 const express = require ("express")
 const cookieParser = require("cookie-parser")
+const cors = require ("cors")
 
 
-const app = express()
+const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors({
+    credentials:true,
+    origin:"http://localhost:5173"
+
+}))
+
 
 /** require router */
 const authRouter = require("./routers/auth.router");
 const PostRouter = require("./routers/post.router");
-const userRouter =require("./routers/user.router");
+const userRouter = require("./routers/user.router");
 
 /**using router */
 app.use("/api/auth", authRouter)
--- Day-14/frountend/src/features/auth/pages/Login.jsx
+++ Day-14/frountend/src/features/auth/pages/Login.jsx
@@ -1,23 +1,65 @@
-import React from 'react'
-import "../style/form.scss"
-
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/form.scss";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios
+      .post(
+        "http://localhost:3000/api/auth/login",
+        {
+          username,
+          password,
+        },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+
   return (
     <main>
-      <div className='form-container'>
-        <h1>Login </h1>
-        <form >
-          <input type="text" name='username' placeholder='Enter usernamae' />
-          <input type="text" name="password" placeholder='Enter password'/>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            onInput={(e) => {
+              setUsername(e.target.value);
+            }}
+            type="text"
+            name="username"
+            placeholder="Enter username"
+          />
 
-          <button type='submit'>Login</button>
+          <input
+            onInput={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            name="password"
+            placeholder="Enter password"
+          />
+
+          <button type="submit">Login</button>
         </form>
+
+        <p>
+          Don't have an account?{" "}
+          <Link className="toggleAuthForm" to="/register">
+            Register
+          </Link>
+        </p>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
