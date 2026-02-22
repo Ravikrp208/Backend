4b15526b3dc24153bb09c3b36f02b102e9e16140
Ravikrp208 <ravikrp208@gmail.com>
2026-02-22 23:58:51 +0530

    Pratice login

--- Day-19/Frontend/src/features/auth/pages/Login.jsx
+++ Day-19/Frontend/src/features/auth/pages/Login.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "../style/form.scss"
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
 
-export default Login
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
+
+
+
+  return (
+
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter password"
+          />
+
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter Password"
+          />
+
+          <button className="button primary-button">Login</button>
+        </form>
+
+      </div>
+    </main>
+  );
+};
+
+export default Login;
