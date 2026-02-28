6c0787e242d6ad7de26d0739c617953aa15a1636
Ravikrp208 <ravikrp208@gmail.com>
2026-02-28 23:47:39 +0530

    pratice backend code to databse connection

---
+++ day-25/Backend/src/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+
+const app = express();
+app.use(express.json());
+
+
+
+
+module.exports = app;
