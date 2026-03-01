0dad5609878132de0fb4a43056168d8a3c03df1c
Ravikrp208 <ravikrp208@gmail.com>
2026-03-01 23:35:07 +0530

    user model

--- day-25/Backend/src/config/database.js
+++ day-25/Backend/src/config/database.js
@@ -1,14 +1,13 @@
 const mongoose = require ("mongoose")
 
- async function connectTodatabase () {
+ async function connectToBD () {
    await mongoose.connect(process.env.MONGO_URI)
    .then(() => {
      console.log("connected to data base");
    })
    .catch((error) => {
      console.log("Error connecting to database:", error);
-     process.exit(1);
    });
 }
 
-module.exports = connectTodatabase
+module.exports = connectToBD
