6697251f2062a83b6f07ec03ab12fbc0686f7784
Ravikrp208 <ravikrp208@gmail.com>
2026-03-09 20:28:11 +0530

    Refactor database connection and user model: Added index drop for typo in MongoDB, corrected userModel references to User in auth controller, and cleaned up user schema formatting.

--- Day 6/src/controllers/auth.controller.js
+++ Day 6/src/controllers/auth.controller.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 
 async function registerUser(req, res) {
     const { username, email, password } = req.body;
-        const isAlreadyExist = await userModel.findOne({
+        const isAlreadyExist = await User.findOne({
         $or: [
             { username },
             { email }
@@ -19,7 +19,7 @@ async function registerUser(req, res) {
      }
 
      const hash = await bcrypt.hash(password, 10);
-     const user = await userModel.create({
+     const user = await User.create({
         username,
         email,
         password: hash
@@ -46,7 +46,7 @@ async function registerUser(req, res) {
 
 async function loginUser(req, res) {
      const { email, password } = req.body;
-     const user = await userModel.findOne({ 
+     const user = await User.findOne({ 
         $or: [
             { email },
             { username: email }         
