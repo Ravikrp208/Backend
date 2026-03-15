66371b5184621c721a0735dfe24bb20f5424bb1a
Ravikrp208 <ravikrp208@gmail.com>
2026-03-15 23:33:42 +0530

    Perplexity create validators

--- Perplexity/Backend/server.js
+++ Perplexity/Backend/server.js
@@ -1,17 +1,20 @@
 import "dotenv/config";
 import app from "./src/app.js";
+import http from "http";
 import connectDB from "./src/config/database.js";
+import { initSocket } from "./src/sockets/server.socket.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
+const httpServer = http.createServer(app);
 
-connectDB() 
- 
-.catch((err) => {
-  console.error("Failed to connect to the database:", err);
-  process.exit(1); // Exit the process with an error code
+initSocket(httpServer);
+
+connectDB().catch((err) => {
+  console.error("MongoDB connection failed:", err);
+  process.exit(1);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
