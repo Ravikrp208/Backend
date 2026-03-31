9c4f12f84670777e361f3d56f92f6b8758fb1142
Ravikrp208 <ravikrp208@gmail.com>
2026-03-31 23:09:27 +0530

    Typescript

---
+++ script.js
@@ -0,0 +1,28 @@
+
+//
+// String + number = string
+// number + string = string
+// string + string = string
+// number + number = number
+
+// 
+//  String - number = number
+//  number - string = number
+//  string - string = NaN
+//  number - number = number
+
+// String * number = number
+// number * string = number
+// string * string = NaN
+// number * number = number
+
+// Array + Array = string
+// Array + string = string
+// string + Array = string
+
+[1] + 1 // "11" 
+[1,2] + 1 // "1,21"
+ 
+Number + Boolean = number
+String + Boolean = string
+console.log("Hello, World!");
