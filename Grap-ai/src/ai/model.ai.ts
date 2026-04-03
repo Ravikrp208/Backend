df9cba91785d719334dbf749f7cf4fd2fd5a709c
Ravikrp208 <ravikrp208@gmail.com>
2026-04-03 23:59:28 +0530

    Langchain

--- Grap-ai/src/ai/model.ai.ts
+++ Grap-ai/src/ai/model.ai.ts
@@ -3,20 +3,17 @@ import { ChatMistralAI } from "@langchain/mistralai";
 import { ChatCohere } from "@langchain/cohere";
 import config from "../config/config.js";
 
-
-
 export const geminiModel = new ChatGoogle({
-    model: "gemini-flash-latest",
-    apiKey: config.GOOGLE_API_KEY,
-  
-}); 
+  model: "gemini-flash-latest",
+  apiKey: config.GOOGLE_API_KEY,
+});
 
 export const mistralAIModel = new ChatMistralAI({
-apiKey: config.MistralAI_API_KEY,
-    model: "mistralai/medium-latest",
+  model: "mistral-medium-latest",
+  apiKey: config.MISTRALAI_API_KEY,
 });
 
 export const cohereModel = new ChatCohere({
-    apiKey: config.CHERE_API_KEY,
-    model: "command-command-a-03-2025",
+  model: "command-a-03-2025",
+  apiKey: config.COHERE_API_KEY,
 });
--- Grap-ai/src/config/config.ts
+++ Grap-ai/src/config/config.ts
@@ -1,14 +1,11 @@
-import { MistralAI } from '@langchain/mistralai';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-export const config = {
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || '',
-    MistralAI_API_KEY: process.env.MISTRALAI_API_KEY || '', 
-    CHERE_API_KEY: process.env.CHERE_API_KEY || '',     
+const config = {
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || "",
+  MISTRALAI_API_KEY: process.env.MISTRAL_API_KEY || "",
+  COHERE_API_KEY: process.env.COHERE_API_KEY || "",
+};
 
-
-}
 export default config;
-  
