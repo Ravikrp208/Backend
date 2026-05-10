e7e2ad790eef3d89eb889d02f7001fa198613eca
Ravikrp208 <ravikrp208@gmail.com>
2026-05-10 23:49:40 +0530

    docker learning

--- Kubernetes/Backend/server.js
+++ Kubernetes/Backend/server.js
@@ -1,14 +1,25 @@
 import express from 'express';
+import morgan from 'morgan';
+
+
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello from the backend!');
+   
+    let sum = 0;
+    for (let i = 0; i < 1000000000; i++) {
+        sum += i;
+    }   
+    res
+    .status(200)
+    .json({ message: 'sum calculated', sum });
 }   
 );
-app.get('/api/data', (req, res) => {
-    res.json({ message: 'This is some data from the backend!' });
-}); 
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+    console.log('Server is running on port 3000');
 });
+
